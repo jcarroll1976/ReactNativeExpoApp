9f3f46deed92d319c1f032a84c029b5f207b23ce
Joshua Carroll <carrolljosh1976@gmail.com>
2024-10-31 22:06:46 -0400

    added header titles for each route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,16 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen 
+        name="index" 
+        options={{
+          headerTitle: "Sticker Smash"
+        }}/>
+      <Stack.Screen
+         name="about"
+         options={{
+          headerTitle: "About"
+        }} />
     </Stack>
   );
 }
