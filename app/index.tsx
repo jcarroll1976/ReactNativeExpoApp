9e1280981ecd349bcf7af3d6893b86a94cd1c68a
Joshua Carroll <carrolljosh1976@gmail.com>
2024-10-31 22:17:56 -0400

    added not found screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
@@ -6,6 +7,7 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.text}>Hello World!</Text>
+      <Link href={"/about"} style={styles.button}>Go to About Screen</Link>
     </View>
   );
 }
@@ -19,5 +21,10 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "white"
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff"
   }
 })
