f5b01757efe2fad180361d9866bba2f4eb229dae
Joshua Carroll <carrolljosh1976@gmail.com>
2024-11-02 03:31:12 -0400

    added image to index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View
       style={styles.container}
     >
+      <Image source={PlaceholderImage} style={styles.image} />
       <Text style={styles.text}>Hello World!</Text>
       
     </View>
@@ -25,5 +29,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: "underline",
     color: "#fff"
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18
   }
 })
